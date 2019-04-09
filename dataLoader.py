808f03d2489f06a04e434fa5b410a61a30affaa2
chris <chrisdulhanty@gmail.com>
2019-04-09 16:55:46 -0400

    changed main to use pytorch dataloader and tensorflow model - fixed the synset id/class/string matching problem

--- dataLoader.py
+++ dataLoader.py
@@ -4,10 +4,16 @@ Author: Chris Dulhanty
 """
 
 import os
+import json
 from torch.utils.data import Dataset
 from skimage import io
 
-class ImageNetDataset(Dataset):
+CLASS_TO_ID_FILE = 'class_to_id.json'
+ID_TO_CLASS_FILE = 'id_to_class.json'
+VAL_LABEL_FILE = 'ILSVRC2012_validation_ground_truth.txt'
+
+
+class ImageNetTrainDataset(Dataset):
     def __init__(self, data_folder, transform=None):
 
         self.data_folder = data_folder
@@ -22,6 +28,9 @@ class ImageNetDataset(Dataset):
                        os.path.isdir(os.path.join(self.data_folder, sub_dir))]
 
         assert len(self.data) == len(self.labels)  # ensure the list comprehensions above worked!
+
+        with open(ID_TO_CLASS_FILE) as f:
+            self.id_to_class_dict = json.load(f)
 
         self.transform = transform
 
@@ -38,18 +47,59 @@ class ImageNetDataset(Dataset):
 
         label = self.labels[idx]
 
-        return image, label
+        sample = {'image': image,
+                  'id': label,
+                  'class': self.id_to_class_dict[label]['class'],
+                  'string': self.id_to_class_dict[label]['string']}
+
+        return sample
+
+
+class ImageNetValidationDataset(Dataset):
+    def __init__(self, data_folder, transform=None):
+
+        self.data_folder = data_folder
+
+        self.data = [f for f in os.listdir(self.data_folder) if os.path.isfile(os.path.join(self.data_folder, f))]
+
+        self.labels = [img.split('_')[0] for img in self.data]
+
+        assert len(self.data) == len(self.labels)  # ensure the list comprehensions above worked!
+
+        with open(CLASS_TO_ID_FILE) as f:
+            self.class_to_id_dict = json.load(f)
+
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+
+        img_path = os.path.join(self.data_folder, self.data[idx])
+        image = io.imread(img_path)
+
+        if self.transform:
+            image = self.transform(image)
+
+        label = self.labels[idx]
+
+        sample = {'image': image, 'label': label}
+
+        return sample
 
 
 if __name__ == '__main__':
     train_data_folder = '/media/chris/Datasets/ILSVRC/imagenet_object_localization/ILSVRC/Data/CLS-LOC/train/'
-    # val_data_folder = '/media/chris/Datasets/ILSVRC/imagenet_object_localization/ILSVRC/Data/CLS-LOC/val/'  TODO fix
+    val_data_folder = '/media/chris/Datasets/ILSVRC/imagenet_object_localization/ILSVRC/Data/CLS-LOC/val/'
 
-    train_dataset = ImageNetDataset(data_folder=train_data_folder)
-    # val_dataset = ImageNetDataset(data_folder=val_data_folder)
+    train_dataset = ImageNetTrainDataset(train_data_folder)
+    val_dataset = ImageNetValidationDataset(val_data_folder)
 
     print(len(train_dataset))
-    # print(len(val_dataset))
+    item = train_dataset.__getitem__(0)
+    print(item['image'].shape, item['id'], item['class'], item['string'])
 
-    image, label = train_dataset.__getitem__(0)
-    print(label, image.shape)
+    print(len(val_dataset))
+    item = val_dataset.__getitem__(0)
+    print(item['label'], item['image'].shape, item['image'].dtype)
